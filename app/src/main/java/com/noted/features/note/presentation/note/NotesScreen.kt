682ca9c77327812ca8087182218c4f3e3614fae3
Regiva <ivanovr98@gmail.com>
2022-11-01 13:57:21 +0400

    extracted composable functions in NotesScreen.kt

--- app/src/main/java/com/noted/features/note/presentation/note/NotesScreen.kt
+++ app/src/main/java/com/noted/features/note/presentation/note/NotesScreen.kt
@@ -12,10 +12,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.noted.R
 import com.noted.core.navigation.Screen
+import com.noted.features.note.domain.model.Note
+import com.noted.features.note.domain.util.NoteOrder
 import com.noted.features.note.presentation.note.components.NoteItem
 import com.noted.features.note.presentation.note.components.OrderSection
 import com.noted.ui.icon.NotedIcons
@@ -29,7 +33,6 @@ fun NotesScreen(
     viewModel: NotesViewModel = hiltViewModel(),
 ) {
     val state by viewModel.stateFlow.collectAsState()
-//    val scaffoldState = rememberScaffoldState() TODO check material3 guides
     val scope = rememberCoroutineScope()
 
     Scaffold(
@@ -39,7 +42,7 @@ fun NotesScreen(
             ) {
                 Icon(
                     imageVector = NotedIcons.Add,
-                    contentDescription = "Add note",
+                    contentDescription = stringResource(R.string.noted_add_note),
                 )
             }
         },
@@ -50,75 +53,108 @@ fun NotesScreen(
                 .fillMaxSize()
                 .padding(16.dp),
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = "Your notes",
-                    style = MaterialTheme.typography.headlineLarge,
-                    color = MaterialTheme.colorScheme.onBackground,
-                )
-                IconButton(
-                    onClick = { viewModel.onEvent(NotesScreenEvents.ToggleOrderSection) },
-                    colors = IconButtonDefaults.iconButtonColors(
-                        contentColor = MaterialTheme.colorScheme.onBackground,
-                    )
-                ) {
-                    Icon(
-                        imageVector = NotedIcons.Sort,
-                        contentDescription = "Sort",
-                    )
-                }
-            }
-            AnimatedVisibility(
-                visible = state.isOrderSectionVisible,
-                enter = fadeIn() + slideInVertically(),
-                exit = fadeOut() + slideOutVertically(),
-            ) {
-                OrderSection(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 16.dp),
-                    noteOrder = state.noteOrder,
-                    onOrderChange = {
-                        viewModel.onEvent(NotesScreenEvents.Order(it))
-                    }
-                )
-            }
+            NotesScreenHeader(
+                onToggleOrder = { viewModel.onEvent(NotesScreenEvents.ToggleOrderSection) }
+            )
+            NotesScreenOrderSection(
+                isOrderSectionVisible = state.isOrderSectionVisible,
+                noteOrder = state.noteOrder,
+                onOrderChange = { noteOrder ->
+                    viewModel.onEvent(NotesScreenEvents.Order(noteOrder))
+                },
+            )
             Spacer(modifier = Modifier.height(16.dp))
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(state.notes) { note ->
-                    NoteItem(
-                        note = note,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                // TODO: open note with animation
-                                // TODO: change params
-                                navController.navigate(
-                                    Screen.AddEditNoteScreen.route +
-                                            "?noteId=${note.id}&noteColor=${note.color}"
-                                )
-                            },
-                        onDeleteClick = {
-                            viewModel.onEvent(NotesScreenEvents.DeleteNote(note))
-                            scope.launch {
-                                val result = snackbarHostState.showSnackbar(
-                                    message = "Note deleted",
-                                    actionLabel = "Undo",
-                                    duration = SnackbarDuration.Short,
-                                )
-                                if (result == SnackbarResult.ActionPerformed) {
-                                    viewModel.onEvent(NotesScreenEvents.RestoreNote)
-                                }
-                            }
+            NotesScreenNotesList(
+                notes = state.notes,
+                onNoteClick = { note ->
+                    // TODO: open note with animation
+                    // TODO: change params
+                    navController.navigate(Screen.AddEditNoteScreen.route +
+                            "?noteId=${note.id}&noteColor=${note.color}")
+                },
+                onDeleteNoteClick = { note ->
+                    viewModel.onEvent(NotesScreenEvents.DeleteNote(note))
+                    scope.launch {
+                        val result = snackbarHostState.showSnackbar(
+                            message = "Note deleted",
+                            actionLabel = "Undo",
+                            duration = SnackbarDuration.Short,
+                        )
+                        if (result == SnackbarResult.ActionPerformed) {
+                            viewModel.onEvent(NotesScreenEvents.RestoreNote)
                         }
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                }
-            }
+                    }
+                },
+            )
         }
+    }
+}
+
+@Composable
+private fun NotesScreenNotesList(
+    notes: List<Note>,
+    onNoteClick: (Note) -> Unit,
+    onDeleteNoteClick: (Note) -> Unit,
+) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(notes) { note ->
+            NoteItem(
+                note = note,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onNoteClick(note) },
+                onDeleteClick = { onDeleteNoteClick(note) },
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
+
+@Composable
+private fun NotesScreenHeader(
+    onToggleOrder: () -> Unit,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = stringResource(R.string.noted_your_notes),
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+        )
+        IconButton(
+            onClick = onToggleOrder,
+            colors = IconButtonDefaults.iconButtonColors(
+                contentColor = MaterialTheme.colorScheme.onBackground,
+            )
+        ) {
+            Icon(
+                imageVector = NotedIcons.Sort,
+                contentDescription = stringResource(R.string.noted_sort),
+            )
+        }
+    }
+}
+
+@Composable
+private fun NotesScreenOrderSection(
+    isOrderSectionVisible: Boolean,
+    noteOrder: NoteOrder,
+    onOrderChange: (NoteOrder) -> Unit,
+) {
+    AnimatedVisibility(
+        visible = isOrderSectionVisible,
+        enter = fadeIn() + slideInVertically(),
+        exit = fadeOut() + slideOutVertically(),
+    ) {
+        OrderSection(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            noteOrder = noteOrder,
+            onOrderChange = onOrderChange
+        )
     }
 }
